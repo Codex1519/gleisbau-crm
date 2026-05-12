ceee28398d87433d22957447e8e01481f442a031
chawasch <chawasch.arsnukajev@gmail.com>
2026-05-12 11:53:01 +0200

    maschinen beendet

--- backend/routers/maschinen.py
+++ backend/routers/maschinen.py
@@ -17,3 +17,43 @@ async def create_maschine(maschine: MaschineCreate, db = Depends(get_db)):
     db.commit()
     db.refresh(neue_maschine)
     return neue_maschine
+
+@router.get("/maschinen/{id}")
+async def read_maschine(id: int, db = Depends(get_db)):
+    maschine = db.query(Maschine).filter(Maschine.id== id).first()
+    if not maschine:
+        raise HTTPException(status_code=404, detail="Maschine nicht gefunden")
+    return maschine
+
+@router.delete("/maschinen/{id}")
+async def delete_maschine(id: int, db = Depends(get_db)):
+    maschine = db.query(Maschine).filter(Maschine.id == id).first()
+    if not maschine:
+        raise HTTPException(status_code=404, detail="Maschine nicht gefunden")
+    db.delete(maschine)
+    db.commit()
+    return {"message": "Maschine gelöscht"}
+
+@router.put("/maschinen/{id}")
+async def update_maschine(id: int, maschine_update: MaschineUpdate, db = Depends(get_db)):
+    maschine = db.query(Maschine).filter(Maschine.id == id).first()
+    if not maschine:
+        raise HTTPException(status_code=404, detail="Maschine nicht gefunden")
+    if maschine_update.name is not None:
+        maschine.typ = maschine_update.typ
+    if maschine.baujahr is not None:
+        maschine.baujahr = maschine_update.baujahr
+    if maschine.status is not None:
+        maschine.status = maschine_update.status
+    if maschine.tuev_datum is not None:
+        maschine.tuev_datum = maschine_update.tuev_datum
+    if maschine.kennzeichen is not None:
+        maschine.kennzeichen = maschine_update.kennzeichen
+    if maschine.naechste_wartung is not None:
+        maschine.naechste_wartung = maschine_update.naechste_wartung
+    db.commit()
+    db.refresh(maschine)
+    return maschine
+
+
+
